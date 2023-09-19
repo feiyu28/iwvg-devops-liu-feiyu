bb2668b277244dc3677ae2d81472530adb24f03f
Feiyu <f.liu@alumnos.upm.es>
2023-09-19 19:38:52 +0200

    enhanced FractionTest #7

--- src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/FractionTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FractionTest {
 
@@ -32,5 +33,45 @@ class FractionTest {
         fraction.setNumerator(4);
         fraction.setDenominator(10);
         assertEquals(0.4, fraction.decimal());
+    }
+
+    @Test
+    void testIsProper() {
+        assertTrue(fraction.isProper());
+    }
+
+    @Test
+    void testIsImproper() {
+        fraction.setNumerator(5);
+        fraction.setDenominator(3);
+        assertTrue(fraction.isImproper());
+    }
+
+    @Test
+    void testIsEquivalent() {
+        assertTrue(fraction.isEquivalent(new Fraction(1, 2)));
+    }
+
+    @Test
+    void testAdd() {
+        fraction.add(new Fraction(1, 3));
+        assertEquals(5, fraction.getNumerator());
+        assertEquals(6, fraction.getDenominator());
+    }
+
+    @Test
+    void testMultiply() {
+        fraction.setNumerator(2);
+        fraction.setDenominator(5);
+        fraction.multiply(new Fraction(2, 3));
+        assertEquals(4, fraction.getNumerator());
+        assertEquals(15, fraction.getDenominator());
+    }
+
+    @Test
+    void testDivide() {
+        fraction.divide(new Fraction(1, 4));
+        assertEquals(4, fraction.getNumerator());
+        assertEquals(2, fraction.getDenominator());
     }
 }
