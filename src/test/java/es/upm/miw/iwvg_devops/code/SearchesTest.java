f9e6ff31c79b13340d1f7e3c62a79fe2dc2db62b
Feiyu <f.liu@alumnos.upm.es>
2023-09-22 01:41:17 +0200

    added search 9 #11

--- src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -1,36 +1,30 @@
 package es.upm.miw.iwvg_devops.code;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SearchesTest {
 
-    private Searches search;
-
-    @BeforeEach
-    void before() {
-        search = new Searches();
-    }
-
     @Test
     void testFindDecimalFractionByNegativeSignFraction() {
-        Object[] found = search.findDecimalFractionByNegativeSignFraction().toArray();
-        assertEquals(3, found.length);
-        assertEquals(-0.2, found[0]);
-        assertEquals(-0.5, found[1]);
-        assertEquals(-0.0, found[2]);
+        assertEquals(List.of(-0.2, -0.5, -0.0),
+                new Searches().findDecimalFractionByNegativeSignFraction().collect(Collectors.toList()));
     }
 
     @Test
     void testFindUserNameBySomeImproperFraction() {
-        Object[] found = search.findUserNameBySomeImproperFraction().toArray();
-        assertEquals(5, found.length);
-        assertEquals("Oscar", found[0]);
-        assertEquals("Ana", found[1]);
-        assertEquals("Oscar", found[2]);
-        assertEquals("Antonio", found[3]);
-        assertEquals("Paula", found[4]);
+        assertEquals(List.of("Oscar", "Ana", "Oscar", "Antonio", "Paula"),
+                new Searches().findUserNameBySomeImproperFraction().collect(Collectors.toList()));
+    }
+
+    @Test
+    void testFindHighestFraction() {
+        Fraction fraction = new Searches().findHighestFraction();
+        assertEquals(2, fraction.getNumerator());
+        assertEquals(1, fraction.getDenominator());
     }
 }
